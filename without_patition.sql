5bb350e76c358d72cb1f5d7224044ad52652e523
iammohan011 <mohanveluduti123@gmail.com>
2023-06-30 12:25:45 +0530

    part_without

---
+++ without_patition.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `mohangcp17.stack_overflow1_dataset.stack_table`
+(
+  id INT64,
+  title STRING,
+  body STRING,
+  accepted_answer_id INT64,
+  answer_count INT64,
+  comment_count INT64,
+  community_owned_date TIMESTAMP,
+  creation_date TIMESTAMP,
+  favorite_count INT64,
+  last_activity_date TIMESTAMP,
+  last_edit_date TIMESTAMP,
+  last_editor_display_name STRING,
+  last_editor_user_id INT64,
+  owner_display_name STRING,
+  owner_user_id INT64,
+  parent_id INT64,
+  post_type_id INT64,
+  score INT64,
+  tags STRING,
+  view_count INT64)
+
